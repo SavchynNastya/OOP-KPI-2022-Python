29d7338f325d7e84a91fff90eb147a4887951618
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-09-14 16:47:12 +0300

    error handling improved

--- Lab1/part1/task1.py
+++ Lab1/part1/task1.py
@@ -1,26 +1,32 @@
 import argparse
 
+
 parser = argparse.ArgumentParser()
 
 # add positional arguments
-parser.add_argument("num1", type=int, help="First number")
+parser.add_argument("num1", type=str, help="First number")
 parser.add_argument("sign", type=str, help="What to do with numbers?")
-parser.add_argument("num2", type=int, help="Second number")
+parser.add_argument("num2", type=str, help="Second number")
 
 args = parser.parse_args()
 # print(args)
 
-if args.sign == '-':
-    print(args.num1 - args.num2)
-elif args.sign == '+':
-    print(args.num1 + args.num2)
-elif args.sign == '/':
-    try:
-        print(args.num1 / args.num2)
-    except ZeroDivisionError:
-        print("Division by zero...")
-elif args.sign == '*':
-    print(args.num1 * args.num2)
-else:
-    print("Please choose one of arithmetic operations: '+', '-', '*', '/'")
-    exit(1)
+try:
+    num1 = int(args.num1)
+    num2 = int(args.num2)
+    if args.sign == '-':
+        print(num1 - num2)
+    elif args.sign == '+':
+        print(num1 + num2)
+    elif args.sign == '/':
+        try:
+            print(num1 / num2)
+        except ZeroDivisionError:
+            print("Division by zero...")
+    elif args.sign == '*':
+        print(num1 * num2)
+    else:
+        print("Please choose one of arithmetic operations: '+', '-', '*', '/'")
+        exit(1)
+except ValueError:
+    print("You entered not a number")
