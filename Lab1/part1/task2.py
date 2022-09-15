ddbbabc716a48ad9d219a929e2122f0d4e5f36c7
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-09-15 17:11:33 +0300

    errors handling improved

--- Lab1/part1/task2.py
+++ Lab1/part1/task2.py
@@ -2,17 +2,29 @@ import argparse
 import operator
 import math
 
+
+def convert_to_float(num_str):
+    try:
+        numbers = list(map(float, num_str))
+        return numbers
+    except ValueError:
+        print("Wrong value type")
+
+
 parser = argparse.ArgumentParser()
 
 # add positional arguments
 parser.add_argument("action", type=str, help="What to do with numbers?")
-parser.add_argument("numbers", type=float, nargs="+")
+parser.add_argument("numbers", type=str, nargs="+")
 
 args = parser.parse_args()
+
+
 # print(args)
 
 func = ''
 try:
+    args.numbers = convert_to_float(args.numbers)
     if hasattr(operator, args.action):
         func = getattr(operator, args.action)
         print(func(*args.numbers))
@@ -25,7 +37,5 @@ except NameError:
     print(f"The function {args.action} is not defined.")
 except TypeError:
     print("Wrong number of arguments")
-except ValueError:
-    print("Please, enter an integer of float number")
 except ZeroDivisionError:
     print("Division by zero...")
