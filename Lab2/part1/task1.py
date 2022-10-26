511c72ba1ccb00de69e332f8131ba1bebf3dc4d5
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-10-26 14:46:28 +0300

    initial commit

---
+++ Lab2/part1/task1.py
@@ -0,0 +1,300 @@
+import json
+import uuid
+import datetime
+
+
+class Person:
+    def __init__(self, name, surname, is_student):
+        self.name = name
+        self.surname = surname
+        self.is_student = is_student
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Name of a person should be a string")
+        self.__name = val
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Surname of a person should be a string")
+        self.__surname = val
+
+    @property
+    def is_student(self):
+        return self.__is_student
+
+    @is_student.setter
+    def is_student(self, val):
+        if not isinstance(val, bool):
+            raise TypeError("Student status of a person should be a boolean")
+        self.__is_student = val
+
+    def __str__(self):
+        return f"{self.name} {self.surname}\n"\
+               f"Student status: {self.is_student}"
+
+
+class RegularTicket:
+    def __init__(self, price):
+        self.id = uuid.uuid4()
+        self.price = price
+
+
+class Advanced(RegularTicket):
+    _ADVANCE_COEF = 0.4  # -40%
+
+    def __init__(self, price):
+        super().__init__(price)
+        self.price = self.price - self.price * self._ADVANCE_COEF
+
+
+class Late(RegularTicket):
+    _LATE_COEF = 0.1  # +10%
+
+    def __init__(self, price):
+        super().__init__(price)
+        self.price = self.price + self.price * self._LATE_COEF
+
+
+class Student(RegularTicket):
+    _STUDENT_COEF = 0.5  # -50%
+
+    def __init__(self, price):
+        super().__init__(price)
+        self.price = self.price - self.price * self._STUDENT_COEF
+
+
+class Event:
+
+    def __init__(self, hour, day, month, year, price, qty, desc=""):
+        self.hour = hour
+        self.day = day
+        self.month = month
+        self.year = year
+        self.desc = desc
+        self.reg_price = price
+        self.tickets_quantity = qty
+
+        self.regular_ticket = RegularTicket(self.reg_price)
+        self.student_ticket = Student(self.reg_price)
+        self.advanced_ticket = Advanced(self.reg_price)
+        self.late_ticket = Late(self.reg_price)
+
+    @property
+    def hour(self):
+        return self.__hour
+
+    @hour.setter
+    def hour(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Hour should be an integer")
+        if not 0 <= val <= 23:
+            raise ValueError("Hour should be 0-23")
+        self.__hour = val
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Day should be an integer")
+        if not 1 <= val <= 31:
+            raise ValueError("Day should be 1-31")
+        self.__day = val
+
+    @property
+    def month(self):
+        return self.__month
+
+    @month.setter
+    def month(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Month should be an integer")
+        if not 1 <= val <= 12:
+            raise ValueError("Month should be 1-12")
+        self.__month = val
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Year should be an integer")
+        if not val >= datetime.date.today().year:
+            raise ValueError("Year should be current or higher")
+        self.__year = val
+
+    @staticmethod
+    def get_datetime(year, month, day, hour):
+        return datetime.datetime(year, month, day, hour)
+
+    @property
+    def reg_price(self):
+        return self.__reg_price
+
+    @reg_price.setter
+    def reg_price(self, val):
+        if not val or not isinstance(val, (int, float)):
+            raise TypeError("Price should be an integer or float")
+        if val < 0:
+            raise ValueError("Price should be > 0")
+        self.__reg_price = val
+
+    @property
+    def tickets_quantity(self):
+        return self.__tickets_quantity
+
+    @tickets_quantity.setter
+    def tickets_quantity(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Tickets quantity should be an integer")
+        if val < 0:
+            raise ValueError("Tickets quantity should be > 0")
+        self.__tickets_quantity = val
+
+    @property
+    def desc(self):
+        return self.__desc
+
+    @desc.setter
+    def desc(self, val):
+        if not isinstance(val, str):
+            raise TypeError("Description should be a string")
+        self.__desc = val
+
+    def ticket_actual_price(self):
+        time_to_event = (self.get_datetime(self.year, self.month, self.day, self.hour)
+                         - datetime.datetime.now()).days
+
+        if time_to_event < 0:
+            return "Sorry, time is up."
+
+        elif time_to_event >= 60:
+            return f"You can buy ticket for ADVANCED price: {self.advanced_ticket.price}\n"\
+                   f"For students - {self.student_ticket.price}\n"
+
+        elif 0 <= time_to_event <= 10:
+            return f"You can buy ticket for LATE price: {self.late_ticket.price}\n" \
+                   f"For students - {self.student_ticket.price}\n"
+
+        else:
+            return f"You can buy ticket for REGULAR price: {self.regular_ticket.price}\n" \
+                   f"For students - {self.student_ticket.price}\n"
+
+    @staticmethod
+    def obtain_ticket(ticket, customer, date):
+        return f"TICKET: {ticket.id} price: {ticket.price}\n"\
+               f"{customer.name} {customer.surname}\n"\
+               f"Date: {date}\n"
+
+    def buy_ticket(self, customer):
+        if not customer or not isinstance(customer, Person):
+            raise TypeError("Customer should be registered")
+        if self.tickets_quantity <= 0:
+            raise ValueError("Sorry, all the tickets are SOLD OUT!")
+
+        time_to_event = (self.get_datetime(self.year, self.month, self.day, self.hour)
+                         - datetime.datetime.now()).days
+        if time_to_event <= 0:
+            raise TimeoutError("Sorry, there is no time left to this event...")
+
+        self.__tickets_quantity -= 1
+        current_time = datetime.datetime.now()
+
+        if customer.is_student:
+            ticket = self.student_ticket
+
+        elif time_to_event >= 60:
+            ticket = self.advanced_ticket
+
+        elif 0 <= time_to_event <= 10:
+            ticket = self.late_ticket
+
+        else:
+            ticket = self.regular_ticket
+
+        with open("bought_tickets.json", 'r') as file:
+            file_data = json.load(file)
+
+        if "bought_tickets" not in file_data:
+            file_data["bought_tickets"] = {}
+        if not str(ticket.id) in file_data["bought_tickets"]:
+            file_data["bought_tickets"][str(ticket.id)] = {}
+            file_data["bought_tickets"][str(ticket.id)]["name"] = customer.name
+            file_data["bought_tickets"][str(ticket.id)]["surname"] = customer.surname
+            file_data["bought_tickets"][str(ticket.id)]["price"] = ticket.price
+            file_data["bought_tickets"][str(ticket.id)]["desc"] = self.desc
+            file_data["bought_tickets"][str(ticket.id)]["date"] = str(current_time)
+
+        with open("bought_tickets.json", 'w') as file:
+            json.dump(file_data, file, indent=4)
+
+        get_ticket = self.obtain_ticket(ticket, customer, current_time)
+        return get_ticket
+
+    @staticmethod
+    def search_ticket(ticket_id):
+        if not ticket_id or not isinstance(ticket_id, str):
+            raise TypeError("Ticket id should be a string")
+
+        with open("bought_tickets.json", 'r') as file:
+            file_data = json.load(file)
+
+        if "bought_tickets" not in file_data:
+            raise KeyError("There is no tickets bought")
+
+        if ticket_id not in file_data["bought_tickets"]:
+            raise KeyError("There is no such ticket bought")
+
+        name = file_data["bought_tickets"][ticket_id]["name"]
+        surname = file_data["bought_tickets"][ticket_id]["surname"]
+        price = file_data["bought_tickets"][ticket_id]["price"]
+        event = file_data["bought_tickets"][ticket_id]["desc"]
+        date = file_data["bought_tickets"][ticket_id]["date"]
+
+        return f"\nTICKET: {ticket_id} price: {price}\n" \
+               f"EVENT: {event}\n"\
+               f"Person: {name} {surname}\n"\
+               f"Date: {date}\n"
+
+
+def main():
+    try:
+        event1 = Event(16, 30, 11, 2022, 350, 20, "Google Event")
+        event2 = Event(12, 6, 3, 2023, 500, 40, "SoftServe Juniors Event")
+
+        print(event1.ticket_actual_price())
+        print(event2.ticket_actual_price())
+
+        customer1 = Person("Anna", "Smith", True)
+        customer2 = Person("Boris", "Muller", False)
+
+        print(event1.buy_ticket(customer1), sep="\n\n")
+        print(event2.buy_ticket(customer2))
+
+        print("Ticket is found:", event1.search_ticket("bee771cc-100b-4731-9761-32c095b75685"))
+
+    except ValueError as e:
+        print(str(e))
+    except TypeError as e:
+        print(str(e))
+    except KeyError as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lab2/part1/task2.py
@@ -0,0 +1,280 @@
+import json
+import datetime
+import uuid
+
+
+class Customer:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.day = datetime.datetime.now().weekday()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Name should be a string")
+        self.__name = val
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Surname should be a string")
+        self.__surname = val
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, val):
+        if not val or not isinstance(val, int):
+            raise TypeError("Weekday should be an integer")
+        if not 0 <= val <= 6:
+            raise ValueError("Day should be 0-6")
+
+        days = {0: "Mon", 1: "Tues", 2: "Wed", 3: "Thur", 4: "Fri", 5: "Sat", 6: "Sun"}
+
+        self.__day = days[val]
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Monday:
+    def __init__(self):
+        with open("pizzas.json", "r") as file:
+            pizza_data = json.load(file)
+        self.day_of_week = "Mon"
+        self.name = pizza_data["pizza-of-the-day"][self.day_of_week]["name"]
+        self.ingredients = pizza_data["pizza-of-the-day"][self.day_of_week]["ingredients"]
+        self.price = pizza_data["pizza-of-the-day"][self.day_of_week]["price"]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Name of the pizza must be a string")
+        self.__name = val
+
+    @property
+    def ingredients(self):
+        return self.__ingredients
+
+    @ingredients.setter
+    def ingredients(self, val):
+        if not val or not isinstance(val, list):
+            raise TypeError("Ingredients of the pizza must be a list")
+        if not all(isinstance(item, str) for item in val):
+            raise ValueError("Each ingredient should be a string")
+        self.__ingredients = val
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, val):
+        if not val or not isinstance(val, (int, float)):
+            raise TypeError("Price of the pizza must be an integer/float")
+        if val <= 0:
+            raise TypeError("Price must be > 0")
+        self.__price = val
+
+    def __str__(self):
+        return f"Day of the week: {self.day_of_week}\n"\
+               f"Today's pizza: {self.name}\n"
+
+
+class Tuesday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Tues"
+
+
+class Wednesday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Wed"
+
+
+class Thursday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Thur"
+
+
+class Friday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Fri"
+
+
+class Saturday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Sat"
+
+
+class Sunday(Monday):
+    def __init__(self):
+        super().__init__()
+        self.day_of_week = "Sun"
+
+
+class Order:
+    __DAYS = {"Mon": Monday(),
+            "Tues": Tuesday(),
+            "Wed": Wednesday(),
+            "Thur": Thursday(),
+            "Fri": Friday(),
+            "Sat": Saturday(),
+            "Sun": Sunday()}
+
+    def __init__(self, customer, *optional_ing):
+        self.id = uuid.uuid4()
+        self.customer = customer
+        self.day = customer.day
+        self.optional_ing = optional_ing
+
+    @property
+    def customer(self):
+        return self.__customer
+
+    @customer.setter
+    def customer(self, val):
+        if not val or not isinstance(val, Customer):
+            raise TypeError("Customer must be an instance of Customer class")
+        self.__customer = val
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, val):
+        if not val or not isinstance(val, str):
+            raise TypeError("Day should be a string")
+        if not self.__DAYS[val]:
+            raise KeyError("There is no such day")
+        self.__day = self.__DAYS[val]
+
+    @property
+    def optional_ing(self):
+        return self.__optional_ing
+
+    @optional_ing.setter
+    def optional_ing(self, val):
+        if not isinstance(val, (str, tuple)):
+            raise TypeError("Ingredients should be a tuple or a string")
+
+        with open("pizzas.json", "r") as file:
+            pizza_data = json.load(file)
+        for ingredient in val:
+
+            ############?????
+            if not pizza_data["optional-ingredients"][ingredient]:
+                raise KeyError("Sorry, there is no such ingredient to add")
+        self.__optional_ing = val
+
+    # def get_check(self, pizza_of_the_day, price):
+    #     return f"CHECK: {self.id}\n"\
+    #            f"Customer: {self.customer}"\
+    #            f"Ordered pizza: {pizza_of_the_day}\n"\
+    #            f"Added ingredients: {str(self.optional_ing) if self.optional_ing else '---'}\n"\
+    #            f"PRICE: {price}\n"\
+    #            f"Date: {datetime.datetime.now()}\n"
+
+    def form_the_order(self):
+        with open("orders.json", "r") as file:
+            orders = json.load(file)
+        if self.id not in orders:
+            orders[self.id] = {}
+            orders[self.id]["customer"] = str(self.customer)
+            orders[self.id]["name"] = self.day.name
+            orders[self.id]["price"] = self.day.price
+            orders[self.id]["ingredients"] = self.day.ingredients
+
+        with open("pizzas.json", "r") as file:
+            pizza_data = json.load(file)
+
+        addititional_price = 0
+        orders[self.id]["additional ingredient"] = list()
+        for ingredient in self.optional_ing:
+            # if ingredient not in pizza_data["optional-ingredients"]:
+            #     raise KeyError("There is no such ingredient to add")
+            orders[self.id]["additional ingredient"].append(ingredient)
+            addititional_price += pizza_data["optional-ingredients"][ingredient]
+
+        orders[self.id]["additional price"] = addititional_price
+        orders[self.id]["summary price"] = addititional_price + self.day.price
+
+        with open("orders.json", "w") as file:
+            json.dump(orders, file, indent=4)
+
+        return f"CHECK: {self.id}\n"\
+               f"Customer: {self.customer}"\
+               f"Ordered pizza: {self.day.name}\n"\
+               f"Added ingredients: {str(self.optional_ing) if self.optional_ing else '---'}\n"\
+               f"PRICE: {self.day.price + addititional_price}\n"\
+               f"Date: {datetime.datetime.now()}\n"
+
+
+
+
+    # def form_the_order(self):
+    #     with open("pizzas.json", "r") as file:
+    #         pizza_data = json.load(file)
+    #     pizza_of_the_day = pizza_data["pizza-of-the-day"][self.day]["name"]
+    #     price = pizza_data["pizza-of-the-day"][self.day]["price"]
+    #
+    #     # ingredients = pizza_data["pizza-of-the-day"][self.day]["ingredients"]
+    #     # ingredients.extend(self.optional_ing)
+    #     # print(ingredients)
+    #
+    #     if self.optional_ing:
+    #         for ingredient in self.optional_ing:
+    #             price += pizza_data["optional-ingredients"][ingredient]
+    #
+    #     check = self.get_check(pizza_of_the_day, price)
+    #     return check
+
+
+def main():
+    try:
+        cust1 = Customer("Anthony", "Perkins")
+        cust2 = Customer("Maria", "Reynold")
+
+        order1 = Order(cust1, "pineapple", "bell pepper")
+        order2 = Order(cust2)
+
+        print(order1.form_the_order())
+        print(order2.form_the_order())
+
+    except ValueError as e:
+        print(str(e))
+    except TypeError as e:
+        print(str(e))
+    except KeyError as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
